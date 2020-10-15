815d4642ea280f4022417da70acda62a4fcf46a8
timur <timur@agilecode.io>
2020-10-15 22:55:08 +0300

    mapping roman numbers to integer

---
+++ 13_roman_to_integer.py
@@ -0,0 +1,13 @@
+def romanToInt(s: str) -> int:
+    result, previous = 0, 0
+    map_roman_to_int = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+
+    for i in s[::-1]:
+        if map_roman_to_int[i] >= previous:
+            result += map_roman_to_int[i]
+        else:
+            result -= map_roman_to_int[i]
+
+        previous = map_roman_to_int[i]
+
+    return result
