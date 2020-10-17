707bbbdeea731be85322d3a0616992b65bc3036f
timur <timur@agilecode.io>
2020-10-17 14:43:12 +0300

    cleanup 26 task

--- 26_remove_duplicatest_from_array.py
+++ 26_remove_duplicatest_from_array.py
@@ -9,9 +9,3 @@ def removeDuplicates(nums: [int]) -> int:
         else:
             index += 1
     return index
-
-
-#nums = [0, 0, 1, 1, 1, 2, 2, 3, 3, 4],
-print(removeDuplicates([0, 0, 1, 1, 1, 2, 2, 3, 3, 4]))
-#    return length = 5,
-#   with the first five elements of nums being modified to 0, 1, 2, 3, and 4 respectively.
